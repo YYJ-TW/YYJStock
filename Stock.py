eda5e54d92ce3ea326034c858f2fa8cd2bf107e6
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2022-05-04 20:40:00 +0800

    新增 未知的股票代碼判斷

--- Stock.py
+++ Stock.py
@@ -1,6 +1,9 @@
 import twstock as stock
 
 code = input('請輸入股票代碼: ')
-search = stock.Stock(code)
-price = search.price[-1:]
-print('收盤價: ', price)
+try:
+    search = stock.Stock(code)
+    price = search.price[-1:]
+    print('收盤價: ', price)
+except KeyError:
+    print('未知的股票代碼!')
