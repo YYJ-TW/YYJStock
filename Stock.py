a11ef42e71549d9478316e7093cafaf658f31ffb
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2023-05-05 20:25:01 +0800

    Get gross profit margin from goodinfo.tw

--- Stock.py
+++ Stock.py
@@ -1,8 +1,6 @@
-
-from ast import expr_context
 import re
-from tracemalloc import stop
 import requests
+import pandas as pd
 import yfinance as yf
 from bs4 import BeautifulSoup
 
@@ -25,10 +23,6 @@ class Get:
                         stock_id = re.search('STOCK_ID=(\d+)', link.get('href')).group(1)
                         return {'stock_id': stock_id + '.TW'}
 
-        if code.isalpha():
-            self.price(code)
-            return {'stock_id': code}
-
     def price(self, code):
         data = yf.download(code , period = '1d', rounding = True)
         open = str(data.iloc[0]['Open'])
@@ -37,11 +31,48 @@ class Get:
         low = str(data.iloc[0]['Low'])
         return {'open': open, 'close': close, 'high': high, 'low': low}
 
-    def financial(self, code):
+    def yf_fin(self, code):
         try:
             data = yf.Ticker(code)
             pe = data.info['trailingPE']
             pe_rounded = '{:.2f}'.format(pe)
-            return {'pe': pe_rounded}
-        except KeyError:
-            return {'pe': '查無本益比資訊'}
+            eps = data.info['bookValue']
+            return {
+                'pe': str(pe_rounded),
+                'bv': str(eps),   
+            }
+        except KeyError as e:
+            print(e)
+            return {
+                'pe': '查無本益比資訊',
+                'bv': '查無EPS資訊',
+            }
+    
+    def goodinfo_fin(self, code, years = 5):
+        if code.endswith('.TW'):
+            code = code.replace('.TW', '')
+        url = f'https://goodinfo.tw/tw/StockBzPerformance.asp?STOCK_ID={code}'
+        print(url)
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'}
+        res = requests.get(url, headers=headers)
+        res.encoding = 'utf-8'
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        table = soup.find('div', {'id': 'txtFinDetailData'}).find('table')
+        rows = table.find_all('tr')
+
+        td_list = []
+        count = 0
+
+        for row in rows:
+            tds = row.find_all('td')
+            if len(tds) > 1:
+                td = tds[12].text.strip()
+                if not re.search('[\u4e00-\u9fff]', td):
+                    td_list.append(td)
+                    count += 1
+                    if count >= years:
+                        break
+        return {
+            'profit': str(td_list),
+        }
--- gui.py
+++ gui.py
@@ -28,17 +28,20 @@ class YYJStock(App):
         code = instance.text
         search = Get().search(code)
         stock_id = search.get('stock_id')
-        price = Get().price(stock_id)
-        financial = Get().financial(stock_id)
-        self.root.ids.label.text = '股票代碼：' + stock_id + '\n開盤：' + price['open'] + '\n收盤：' + price['close'] + '\n最高：' + price['high'] + '\n最低：' + price['low']
-        self.root.ids.financial.text = str('目前本益比：' + financial['pe'])
-        try:
+        if stock_id == '暫時無法讀取資料':
+            self.root.ids.img.source = 'oops.png'
+            self.root.ids.img.reload()
+        else:
+            get = Get()
+            price = get.price(stock_id)
+            yf_fin = get.yf_fin(stock_id)
+            fin = get.goodinfo_fin(stock_id)
+            self.root.ids.label.text = '股票代碼：' + stock_id + '\n開盤：' + price['open'] + '\n收盤：' + price['close'] + '\n最高：' + price['high'] + '\n最低：' + price['low']
+            self.root.ids.financial.text = '目前本益比：' + yf_fin['pe'] + '\n每股淨值' + yf_fin['bv'] + '\n毛利率' + fin['profit']
             generate_chart(search.get('stock_id'))
             self.root.ids.img.source = 'chart.png'
             self.root.ids.img.reload()
-        except:
-            self.root.ids.img.source = 'oops.png'
-            self.root.ids.img.reload()
+            
 
 if __name__ == '__main__':
     YYJStock().run()
