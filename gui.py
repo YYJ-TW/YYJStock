94fcf5b6bfd5a9daf90d1bde27f1e4c9bd103040
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2023-05-08 13:00:20 +0800

    Redesign images

--- gui.py
+++ gui.py
@@ -25,6 +25,10 @@ class YYJStock(App):
     def build(self):
         return MyLayout()
 
+    def img(self, source):
+        self.root.ids.img.source = source
+        self.root.ids.img.reload()
+
     def on_enter(self, instance):
         code = instance.text
         get = Get()
@@ -32,26 +36,28 @@ class YYJStock(App):
         stock_id = get.stock_id
         stock_type = get.stock_type
         
-        if stock_id == '暫時無法讀取資料':
-            self.root.ids.img.source = 'oops.png'
-            self.root.ids.img.reload()
+        if stock_id is None: # Not found
+            self.img('img/not-found.png')
         else:
             search = Get().search(code)
             stock_id = search.get('stock_id')
-
-            self.basic_info(stock_id, stock_type)
-            Chart().generate_chart(stock_id, 60)
-            self.root.ids.img.source = 'chart.png'
-            self.root.ids.img.reload()
+            try:
+                self.basic_info(stock_id, stock_type)
+                Chart().generate_chart(stock_id, 60)
+                self.img('img/chart.png')
+            except Exception as e: # No Internet
+                self.img('img/no-internet.png')
+                print(e)
 
     def basic_info(self, stock_id, stock_type):
         get = Get()
         price = get.price(stock_id)
         yf_fin = get.yf_fin(stock_id)
-        fin = get.goodinfo_fin(stock_id, n = 12)
+        fin = get.goodinfo_fin(stock_id, [3], 5)
+        print(fin)
 
         label_text = f'股票代碼：{stock_id}\n公司類型：{stock_type}\n開盤：{price["open"]}\n收盤：{price["close"]}\n最高：{price["high"]}\n最低：{price["low"]}'
-        fin_text = f'目前本益比：{yf_fin["pe"]}\n每股淨值：{yf_fin["bv"]}\n毛利率：{fin["profit"]}'
+        fin_text = f'目前本益比：{yf_fin["pe"]}\n每股淨值：{yf_fin["bv"]}\n基本財報：\n{fin}'
 
         self.root.ids.label.text = label_text
         self.root.ids.financial.text = fin_text
