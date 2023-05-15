01d5eb3ede4a2e4f91ef94b8f6e25928b3407892
YYJ <68520446+YYJ-TW@users.noreply.github.com>
2023-05-15 15:15:51 +0800

    Use KivyMD style

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ from stock import Get
 from chart import Chart
 from analyze import Analyze
 # GUI
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.tabbedpanel import TabbedPanel
 
@@ -21,8 +21,10 @@ Builder.load_file('kivy.kv')
 class MyLayout(TabbedPanel):
     pass
 
-class YYJStock(App):
+class YYJStock(MDApp):
     def build(self):
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.primary_palette = 'Gray'
         return MyLayout()
 
     def img(self, source):
@@ -34,6 +36,7 @@ class YYJStock(App):
         get = Get()
         get.search(code)
         stock_id = get.stock_id
+        self.a_stock_id = get.stock_id
         stock_name = get.stock_name
         stock_type = get.stock_type
         
@@ -42,28 +45,35 @@ class YYJStock(App):
         else:
             search = Get().search(code)
             stock_id = search.get('stock_id')
-            try:
-                self.basic_info(stock_id, stock_name, stock_type)
-                Chart().generate_chart(stock_id, 60)
-                self.img('img/chart.png')
-            except Exception as e: # No Internet
-                self.img('img/no-internet.png')
-                print(e)
+            self.basic_info(stock_id, stock_name, stock_type)
+            self.analyze(stock_id)
+            Chart().generate_chart(stock_id, 60)
+            self.img('img/chart.png')
+            # self.img('img/no-internet.png')
 
     def basic_info(self, stock_id, stock_name, stock_type):
         get = Get()
         price = get.price(stock_id)
         yf_fin = get.yf_fin(stock_id)
         fin = get.goodinfo_fin(stock_id, [0, 12, 13, 14, 15], 5)
-        analyze = Analyze()
         print(fin)
-
+            
         label_text = f'股票代碼：{stock_id}\n股票名稱：{stock_name}\n公司類型：{stock_type}\n開盤：{price["open"]}\n收盤：{price["close"]}\n最高：{price["high"]}\n最低：{price["low"]}'
         fin_text = f'目前本益比：{yf_fin["pe"]}\n每股淨值：{yf_fin["bv"]}\n基本財報：\n{fin}'
-        analyze_text = f'相比去年毛利率是{analyze.basic_fin(fin, 1)}\n相比去年營業利益是{analyze.basic_fin(fin, 2)}'
 
-        self.root.ids.label.text = label_text
+        self.root.ids.label_text.text = label_text
         self.root.ids.financial.text = fin_text
+
+    def update(self, value):
+        print(value)
+        YYJStock().analyze(self.a_stock_id, int(value))
+        
+    def analyze(self, stock_id, years = 2):
+        get = Get()
+        analyze = Analyze()
+        
+        fin = get.goodinfo_fin(stock_id, [0, 12, 13, 14, 15], 5)
+        analyze_text = f'相比去年毛利率是{analyze.basic_fin(fin, 1, years)}\n相比去年營業利益是{analyze.basic_fin(fin, 2, years)}'
         self.root.ids.analyze.text = analyze_text
 
 if __name__ == '__main__':
